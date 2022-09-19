c5cb0c00504bbaf4472742a67027866b9c736775
lescribao <lescribaoliva@gmail.com>
2022-09-19 23:36:32 +0200

    added pokemon services for URLs

--- pokemon-ng/src/app/models/pokemon.ts
+++ pokemon-ng/src/app/models/pokemon.ts
@@ -1,9 +1,46 @@
 export class Pokemon {
 
-    constructor(pokedexId: number,name: string, hp: number,
-        attack: number,
-        defense: number,
-        sattack: number,
-        sdefense: number, image: string)
-
-}
+    constructor(private _id: number, private _name: string, private _abilities: number[], private _type: string[], private _sprites: string[]) {
+    }
+  
+  
+    get id(): number {
+      return this._id;
+    }
+  
+    set id(value: number) {
+      this._id = value;
+    }
+  
+    get name(): string {
+      return this._name;
+    }
+  
+    set name(value: string) {
+      this._name = value;
+    }
+  
+    get abilities(): number[] {
+      return this._abilities;
+    }
+  
+    set abilities(value: number[]) {
+      this._abilities = value;
+    }
+  
+    get type(): string[] {
+      return this._type;
+    }
+  
+    set type(value: string[]) {
+      this._type = value;
+    }
+  
+    get sprites(): string[] {
+      return this._sprites;
+    }
+  
+    set sprites(value: string[]) {
+      this._sprites = value;
+    }
+  }
--- pokemon-ng/src/app/service/pokemon.service.ts
+++ pokemon-ng/src/app/service/pokemon.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private readonly baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  private readonly baseUrl2 = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(currentPage: String): Observable<pokeList> {
+    return this.http.get<pokeList>(currentPage);
+  }
+
+  getPokemonDetails(pokemonUrl: string): Observable<Pokemon> {
+    return this.http.get<Pokemon>(pokemonUrl);
+  }
+
+  addPokemon(pokemon: Pokemon): Observable<any> {
+    const headers = { 'content-type': 'application/json'}
+    const body=JSON.stringify(pokemon);
+    console.log(body)
+    return this.http.post(this.baseUrl2 + '/add-pokemon', body, {'headers':headers})
+
+  }
 }
 export interface pokeList {
   next: string;
   previous: string;
-  results: Array<pokemon>;
+  results: Array<Pokemon>;
 }
 
 /*
@@ -20,7 +41,7 @@ export interface poke {
 }
 */
 
-export interface pokemon {
+export interface Pokemon {
 
   name: string;
   sprites: {
